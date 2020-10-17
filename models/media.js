9fbb49534396d310e908ce619119634ed855cd88
Husin <Muhamad.husin0412@gmail.com>
2020-10-17 10:09:08 +0700

    service-media

---
+++ models/media.js
@@ -0,0 +1,29 @@
+
+module.exports = (sequelize, DataTypes) => {
+    const Media = sequelize.define('Media', {
+        // id: {
+        //     type: DataTypes.INTEGER,
+        //     primaryKey: true,
+        //     autoIncement: true,
+        //     allowNull: true
+        // },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        createdAt: {
+            field: 'created_at',
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        updatedAt: {
+            field: 'updated_at',
+            type: DataTypes.DATE,
+            allowNull: false
+        }
+    }, {
+        tableName: 'media'
+    });
+
+    return Media;
+}
